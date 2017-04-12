e494ad4e7fa1a5d40a13727fc3bb0bea740e09a3
Sergii Ivanchenko <ivanchenko.s@gmail.com>
2017-04-12 15:28:10 +0300

    Create functions getCatergories. Options inputs created by each category

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     function () {
         "use strict";
 
-        angular.module ('combinatorMdl',[])
+        angular.module ('combinatorMdl', [])
                .controller ('InputController', InputController)
                .service ('sentenceAnalysisService', sentenceAnalysisService);
 
@@ -16,6 +16,57 @@
             var inputCtrl = this;
             inputCtrl.isSentenceSubmited = false;
             inputCtrl.showOptions = false;
+            inputCtrl.categoryOptions = [[""]];
+            inputCtrl.categories = [];
+            inputCtrl.sentencesOptions = [];
+
+
+            inputCtrl.userSentence = "Above Grade Level [delivery method] quality tutoring assistance with some of the best [subject] tutors in [location].";
+
+            inputCtrl.clearOptions = function () {
+                inputCtrl.isSentenceSubmited = false;
+                inputCtrl.showOptions = false;
+                inputCtrl.categoryOptions = [[""]];
+
+                while (inputCtrl.categories.length > 0) {
+                    inputCtrl.categories.pop ();
+                }
+                while (inputCtrl.sentencesOptions.length > 0) {
+                    inputCtrl.sentencesOptions.pop ();
+                }
+            };
+
+
+            inputCtrl.SubmitSentence = function () {
+                if (inputCtrl.userSentence) {
+
+                    inputCtrl.categories = sentenceAnalysisService.getCatergories (inputCtrl.userSentence);
+                    initOptionsArray (inputCtrl.categories);
+                    inputCtrl.isSentenceSubmited = true;
+                }
+            };
+
+            inputCtrl.SubmitOptions = function () {
+                console.log (inputCtrl.categories);
+                inputCtrl.showOptions = true;
+                // inputCtrl.sentencesOptions = sentenceAnalysisService.createSentencesOptions (inputCtrl.userSentence, inputCtrl.categories);
+
+            };
+
+            inputCtrl.addOption = function (categoryIndex, optionValue) {
+
+                inputCtrl.categories[categoryIndex].categoryValues.push (optionValue);
+                inputCtrl.categoryOptions[categoryIndex].push (optionValue);
+
+                console.log (inputCtrl.categories[categoryIndex]);
+            };
+
+            var initOptionsArray = function (categoriesArray) {
+                for (var i = 0; i < inputCtrl.categories.length; i++) {
+                    inputCtrl.categories[i].categoryValues = [];
+                    inputCtrl.categoryOptions[i] = [""];
+                }
+            };
 
 
         }
@@ -23,7 +74,43 @@
 
         function sentenceAnalysisService() {
             var service = this;
+            var categoriesObj = [];
+            var sentencesOptions = [];
 
+            service.getCatergories = function (sentence) {
+
+                var categorisArray = sentence.match (/[^[\]]+(?=])/g);
+
+
+                for (var i = 0; i < categorisArray.length; i++) {
+                    var obj = {categoryName: categorisArray[i]};
+                    categoriesObj.push (obj);
+
+                }
+                console.log (categoriesObj);
+                return categoriesObj;
+            };
+
+            service.createSentencesOptions = function (sentence, categories) {
+
+                sentencesOptions.push (sentence);
+                
+                for (var i = 0; i < categories.length; i++) {
+                    // categories[i].categoryName;
+
+                    for (var n = 0; n < categories[i].categoryValues.length; n++) {
+                       // [n];
+                        var res = sentence.replace("["+categories[i].categoryName+"]", "---OPTION---");
+                        console.log(res);
+
+                    }
+
+
+                  
+                }
+                
+                return sentencesOptions;
+            }
 
 
         }
