0e11e894d49dac6b3e08e5d4f4d4a7ffc60b41c7
Sergii Ivanchenko <ivanchenko.s@gmail.com>
2017-04-12 11:00:38 +0300

    Initial html elements setuped. InputController and sentenceAnalysisService initialized

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 /**
  * Created by sergiiivanchenko on 12/04/2017.
  */
+(
+    function () {
+        "use strict";
+
+        angular.module ('combinatorMdl',[])
+               .controller ('InputController', InputController)
+               .service ('sentenceAnalysisService', sentenceAnalysisService);
+
+
+        InputController.$inject = ['sentenceAnalysisService'];
+
+        function InputController(sentenceAnalysisService) {
+            var inputCtrl = this;
+            inputCtrl.isSentenceSubmited = false;
+            inputCtrl.showOptions = false;
+
+
+        }
+
+
+        function sentenceAnalysisService() {
+            var service = this;
+
+
+
+        }
+
+    }
+) ();
